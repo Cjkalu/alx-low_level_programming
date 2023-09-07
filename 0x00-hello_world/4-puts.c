04f45ec9f166d78058d6b94ff793db31ec263f92
Cjkalu <cjaykalu@gmail.com>
2023-09-07 08:14:51 -0700

    puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+//This is a simple C program that prints the strings on the output
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle ");
 	return (0);
 }
