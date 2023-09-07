309a564223014f105be88e192913c1d60013309f
Cjkalu <cjaykalu@gmail.com>
2023-09-07 09:29:46 -0700

    printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* 5-printf.c: Another simple program to print with the printf function*/
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This repository is regarded as my genesis of C programming language, the day I started C as a beginner
